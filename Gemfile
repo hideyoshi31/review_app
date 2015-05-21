e208ecff5f306c2980c3ff5696bcf90a0a4e63f1
hideyoshi31 <matsukawa5955@gmail.com>
2015-05-21 23:13:27 +0900

    local environment

--- Gemfile
+++ Gemfile
@@ -10,8 +10,7 @@ gem 'devise'
 gem 'pry'
 gem 'pry-rails'
 
-# Use mysql as the database for Active Record
-gem 'mysql2'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -43,10 +42,18 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
+  gem 'sqlite3'
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+group :production do
+  # Use mysql as the database for Active Record
+  gem 'mysql2'
+end
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -14,26 +14,26 @@
 ActiveRecord::Schema.define(version: 20150516114256) do
 
   create_table "admin_users", force: :cascade do |t|
-    t.string   "email",               limit: 255, default: "", null: false
-    t.string   "encrypted_password",  limit: 255, default: "", null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",  limit: 255
-    t.string   "last_sign_in_ip",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "star",       limit: 4
-    t.text     "comment",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name"
+    t.integer  "star"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
