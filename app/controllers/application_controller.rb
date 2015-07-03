f7f47305a6c0e94aa4a62ff8dd0232b36dab4d64
hideyoshi31 <matsukawa5955@gmail.com>
2015-07-04 07:06:16 +0900

    hidden not currentuser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  helper :all # include all helpers, all the time
+  helper_method :me?
+
+  protected
+
+  # @user を設定
+  def set_user
+    current_user
+    if params[:user]
+      if user = User.find_by_login(params[:user])
+        @user = user
+      else
+        render :text => 'user not found' and return
+      end
+    else
+      @user = @current_user
+    end
+  end
+
+  # @userが自分かどうか
+  def me?
+    @user == @current_user
+  end
 end
