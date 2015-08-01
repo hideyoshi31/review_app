54e09f1be08c513c52de3e83d105e5040ce1c6c5
hideyoshi31 <matsukawa5955@gmail.com>
2015-08-01 11:45:50 +0900

    ACAO2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :allow_cross_domain_access
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+  end
 
   helper :all # include all helpers, all the time
   helper_method :me?
