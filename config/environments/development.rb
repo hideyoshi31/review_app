6a0689cba93f55a2b610bdd1c15d5209886a4807
hideyoshi31 <matsukawa5955@gmail.com>
2015-08-02 10:16:16 +0900

    mail認証

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,5 +40,14 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+:address => 'turtlecat.xsrv.jp',
+:port => 587,
+:authentication => :plain,
+:openssl_verify_mode => 'none',
+:user_name => 'support@pine.jpn.com', # 送信するメールアドレス
+:password => 'toshiya5955' #password
+}
 
 end
